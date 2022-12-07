6570f3aeee7e7a18b3db4f493f0f52ad316046f6
Tim Heurich <tim.heurich@idealo.de>
2022-12-07 21:03:17 +0100

    refactor(SHUTTLE-912): Cleanup spaghetti code and code duplication

--- jira/config.go
+++ jira/config.go
@@ -13,13 +13,16 @@ type Config struct {
 	jiraClient *jira.Client
 }
 
-type AddHeaderTransport struct {
+type AuthTransport interface {
+	Client() *http.Client
+}
+type CustomAuthHeaderTransport struct {
 	customAuthHeaderKey   string
 	customAuthHeaderValue string
 	Transport             http.RoundTripper
 }
 
-func (adt *AddHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+func (adt *CustomAuthHeaderTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.Header.Add(adt.customAuthHeaderKey, adt.customAuthHeaderValue)
 	return adt.Transport.RoundTrip(req)
 }
@@ -27,43 +30,60 @@ func (adt *AddHeaderTransport) RoundTrip(req *http.Request) (*http.Response, err
 func (c *Config) createAndAuthenticateClient(d *schema.ResourceData) error {
 	log.Printf("[INFO] creating jira client using environment variables")
 
-	transport := AddHeaderTransport{}
+	customTransport := buildCustomHeader(d)
+
+	isBasicAuth := d.Get("username") != nil && d.Get("password") != nil
+	isPatAuth := d.Get("pat_token") != nil
+
+	switch {
+	case isBasicAuth:
+		return createBasicAuthClient(d, customTransport, c)
+	case isPatAuth:
+		return createPatClient(d, customTransport, c)
+	case isPatAuth && isBasicAuth:
+		return errors.New("Only one client type allowed. Can not create two clients, BasicAuth or PAT Auth.")
+	default:
+		return errors.New("Could not create a JIRA client, either set 'username' and 'password' or 'pat_token'")
+	}
+}
+
+func createBasicAuthClient(d *schema.ResourceData, customTransport CustomAuthHeaderTransport, c *Config) error {
+	tp := jira.BasicAuthTransport{
+		Username:  d.Get("username").(string),
+		Password:  d.Get("password").(string),
+		Transport: &customTransport,
+	}
+
+	return createClient(d, &tp, c)
+}
+func createPatClient(d *schema.ResourceData, customTransport CustomAuthHeaderTransport, c *Config) error {
+	tp := jira.BearerAuthTransport{
+		Token:     d.Get("pat_token").(string),
+		Transport: &customTransport,
+	}
+
+	return createClient(d, &tp, c)
+}
+
+func createClient(d *schema.ResourceData, tp AuthTransport, c *Config) error {
+	jiraClient, err := jira.NewClient(tp.Client(), d.Get("url").(string))
+	if err != nil {
+		return errors.Wrap(err, "creating jira client failed")
+	}
+
+	c.jiraClient = jiraClient
+	return nil
+}
+
+func buildCustomHeader(d *schema.ResourceData) CustomAuthHeaderTransport {
+	transport := CustomAuthHeaderTransport{}
 
 	if d.Get("custom_auth_header_key") != nil && d.Get("custom_auth_header_value") != nil {
-		transport = AddHeaderTransport{
+		transport = CustomAuthHeaderTransport{
 			customAuthHeaderKey:   d.Get("custom_auth_header_key").(string),
 			customAuthHeaderValue: d.Get("custom_auth_header_value").(string),
 			Transport:             http.DefaultTransport,
 		}
 	}
-
-	if d.Get("pat_token") != nil {
-		tp := jira.BearerAuthTransport{
-			Token:     d.Get("pat_token").(string),
-			Transport: &transport,
-		}
-
-		jiraClient, err := jira.NewClient(tp.Client(), d.Get("url").(string))
-		if err != nil {
-			return errors.Wrap(err, "creating jira client failed")
-		}
-
-		c.jiraClient = jiraClient
-
-	} else if d.Get("username") != nil && d.Get("password") != nil {
-		tp := jira.BasicAuthTransport{
-			Username:  d.Get("username").(string),
-			Password:  d.Get("password").(string),
-			Transport: &transport,
-		}
-
-		jiraClient, err := jira.NewClient(tp.Client(), d.Get("url").(string))
-		if err != nil {
-			return errors.Wrap(err, "creating jira client failed")
-		}
-
-		c.jiraClient = jiraClient
-	}
-
-	return nil
+	return transport
 }
